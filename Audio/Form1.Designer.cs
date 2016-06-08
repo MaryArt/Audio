21a529edbad2632a99e1016c4005517104a27987
Mary <mariya.vorobyova94@gmail.com>
2016-06-08 21:22:31 +0300

    First commit

---
+++ Audio/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+
+namespace Audio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.waveViewer1 = new NAudio.Gui.WaveViewer();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(121, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // waveViewer1
+            // 
+            this.waveViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.waveViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.waveViewer1.Location = new System.Drawing.Point(12, 56);
+            this.waveViewer1.Name = "waveViewer1";
+            this.waveViewer1.SamplesPerPixel = 128;
+            this.waveViewer1.Size = new System.Drawing.Size(632, 155);
+            this.waveViewer1.StartPosition = ((long)(0));
+            this.waveViewer1.TabIndex = 2;
+            this.waveViewer1.WaveStream = null;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(202, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Open File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 217);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(632, 165);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 394);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.waveViewer1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Audio";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private NAudio.Gui.WaveViewer waveViewer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Audio/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.FileFormats;
+using NAudio.CoreAudioApi;
+using NAudio;
+
+namespace Audio
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Stream for record
+        /// </summary>
+        WaveIn waveIn;
+
+        /// <summary>
+        /// Class for writing in file
+        /// </summary>
+        WaveFileWriter writer;
+
+        /// <summary>
+        /// Name of file
+        /// </summary>
+        string outputFilename = "NoName.wav";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Get data from input buffer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(waveIn_DataAvailable), sender, e);
+            }
+            else
+            {
+                //Writing data in file from buffer
+                writer.WriteData(e.Buffer, 0, e.BytesRecorded);
+            }
+        }
+
+
+        //Stop recording
+        private void waveIn_RecordingStopped(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler(waveIn_RecordingStopped), sender, e);
+            }
+            else
+            {
+                waveIn.Dispose();
+                waveIn = null;
+                writer.Close();
+                writer = null;
+            }
+        }
+        
+        /// <summary>
+        /// Start recording
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var saveDialog = new SaveFileDialog {Filter = "WAVE| *.wav"};
+                if (saveDialog.ShowDialog() != DialogResult.OK) return;
+                outputFilename = saveDialog.FileName;
+
+                waveIn = new WaveIn();
+                //Number device of recording by default (laptop microphone = 0)
+                waveIn.DeviceNumber = 0;
+                //Attach to event the DataAvailable handler, arising  if  the write data is
+                waveIn.DataAvailable += waveIn_DataAvailable;
+                //Attach handler of stoping of a record 
+                waveIn.RecordingStopped += new EventHandler<StoppedEventArgs>(waveIn_RecordingStopped);
+                //Format of the file - get parametrs - a sampling frequency and number of channels(mono)
+                waveIn.WaveFormat = new WaveFormat(8000, 1);
+                //Initialize the object WaveFileWriter
+                writer = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
+                //Start recording
+                waveIn.StartRecording();
+            }
+            catch (Exception ex) //Bad practice X-/
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Break the recording
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            waveIn?.StopRecording();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog {Filter = "Wave file (*.wav)|*.wav"};
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            chart1.Series.Add("wave");
+            chart1.Series["wave"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            chart1.Series["wave"].ChartArea = "ChartArea1";
+            
+            var wave = new WaveChannel32(new WaveFileReader(openFileDialog.FileName));
+            byte[] buffer = new byte[16384];
+            int read = 0;
+            while (wave.Position < wave.Length)
+            {
+                read = wave.Read(buffer, 0, 16384);
+                for (int i = 0; i<read/4; i++)
+                {
+                    chart1.Series["wave"].Points.Add(BitConverter.ToSingle(buffer, i*4));
+                }
+            }
+            waveViewer1.WaveStream = new NAudio.Wave.WaveFileReader(openFileDialog.FileName);
+
+        }
+    }
+}
